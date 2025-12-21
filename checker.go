2316bc04ea85b6faec43f207ce938375fddcbc66
Delakzz <wizpizz.jaoberonio@gmail.com>
2025-12-21 15:28:58 +0800

    updated checker.go

--- checker.go
+++ checker.go
@@ -2,15 +2,31 @@ package main
 
 import "strings"
 
-func Check(guess, target string) (int, int, bool) {
+// Check compares the player's guess with the target number in the game
+// and returns the number of hits and bulls-eye
+//
+// Parameters:
+//   - guess  : The player's guessed number as a string.
+//   - target : The target number to be guessed as a string.
+//
+// Returns:
+//   - hit       : The count of digits that exist in the target but are in the wrong position.
+//   - bullsEye  : The count of digits that are correct and in the correct position.
+//
+// Behavior:
+//   - If the guess is exactly the same as the target, the function returns the length
+//     of the target as bulls-eye, and 0 hits.
+//   - Otherwise, it counts bulls-eye first (correct digits in the correct position),
+//     then counts hits (correct digits in the wrong position) using a map for efficiency.
+func Check(guess, target string) (int, int) {
 	hit, bullsEye := 0, 0
 
-	// check if the user guessed the target random number correctly
+	// return immediately if guess exactly matches target
 	if x := strings.Compare(guess, target); x == 0 {
-		return len(target), 0, true
+		return len(target), 0
 	}
 
-	// create maps for easier checking
+	// create map for easier checking
 	m := make(map[string]int)
 
 	// count bullsEye
@@ -23,15 +39,18 @@ func Check(guess, target string) (int, int, bool) {
 		}
 
 		// add to maps
-		m[y] = i
+		// +1 because 0 is used to check if the number exists
+		m[y] = i + 1
 	}
 
 	// count hit
 	for i := 0; i < len(guess); i++ {
+		// num != 0: exists
+		// num != i+1: not in the same position
 		if num := m[string(guess[i])]; num != 0 && num != i+1 {
 			hit += 1
 		}
 	}
 
-	return hit, bullsEye, false
+	return hit, bullsEye
 }
--- main.go
+++ main.go
@@ -1,9 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	for i := 0; i < 5; i++ {
-		fmt.Println(GenerateRandomNumber(4))
+	scanner := bufio.NewScanner(os.Stdin)
+	attempts := 0
+	target := GenerateRandomNumber(4)
+	guessed := false
+
+	fmt.Println("Welcome to the Code Cracker!")
+	fmt.Println("I'm thinking of a 4-digit number.")
+	fmt.Println("You have 5 chances to guess the correct number.")
+	fmt.Println()
+
+	for attempts < 5 {
+		fmt.Print("Enter your guess: ")
+		scanner.Scan()
+		guess := scanner.Text()
+		hit, bullsEye := Check(guess, target)
+		attempts += 1
+
+		if bullsEye == 4 {
+			fmt.Printf("Congratulations! You guessed the correct number in %d attempts.\n", attempts)
+			guessed = true
+			break
+		} else {
+			fmt.Printf("Incorrect! You got %d bullseyes and %d hits.\n\n", bullsEye, hit)
+		}
+
+	}
+
+	if !guessed {
+		fmt.Printf("The target number is %s!!\n", target)
 	}
 }
